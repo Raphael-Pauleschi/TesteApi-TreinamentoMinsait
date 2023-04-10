253982e09f1d974ae035a894334ce17364518814
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-04-10 19:18:09 -0300

    Ternay operations instead of if statements

--- emprestimo/src/main/java/com/minsait/emprestimo/service/ClientService.java
+++ emprestimo/src/main/java/com/minsait/emprestimo/service/ClientService.java
@@ -17,56 +17,47 @@ public class ClientService {
 	public ClientService(ClientRepository clientRepository) {
 		this.clientRepository = clientRepository;
 	}
-	
+
 	public Client registerClient(Client client) {
 		Client clientReturn = this.clientRepository.save(client);
 		return clientReturn;
 	}
-	
-	public List<Client> returnAllClients(){
+
+	public List<Client> returnAllClients() {
 		return this.clientRepository.findAll();
 	}
 
-	public Client returnOneClient(String cpf) throws ClientNotFoundException{
-		if  (this.clientRepository.existsById(cpf)) {
-		Client clientReturn = this.clientRepository.findById(cpf).get();
-		return clientReturn;}
-		
+	public Client returnOneClient(String cpf) throws ClientNotFoundException {
+		if (this.clientRepository.existsById(cpf)) {
+			Client clientReturn = this.clientRepository.findById(cpf).get();
+			return clientReturn;
+		}
+
 		throw new ClientNotFoundException(cpf);
-		
+
 	}
 
 	public void deleteClient(String cpf) {
 		clientRepository.deleteById(cpf);
-		
+
 	}
 
 	public Client updateClient(String cpf, Client client) throws ClientNotFoundException {
 		Client clientToBeModified = returnOneClient(cpf);
-		
+
 		client.setCpf(cpf);
-		
-		if(client.getMonthlyIncome() == null) {
-			client.setMonthlyIncome(clientToBeModified.getMonthlyIncome());
-		}
-		
-		if(client.getPhoneNumber() == null) {
-			client.setPhoneNumber(clientToBeModified.getPhoneNumber());
-		}
-		
-		if (client.getStreetName() == null) {
-			client.setStreetName(clientToBeModified.getStreetName());
-		}
-		
-		if(client.getStreetNumber() == null) {
-			client.setStreetNumber(clientToBeModified.getStreetNumber());
-		}
-		
-		if(client.getCep()==null) {
-			client.setCep(clientToBeModified.getCep());
-		}
-		
+
+		client.setMonthlyIncome(
+				client.getMonthlyIncome() == null ? clientToBeModified.getMonthlyIncome() : client.getMonthlyIncome());
+		client.setPhoneNumber(
+				client.getPhoneNumber() == null ? clientToBeModified.getPhoneNumber() : client.getPhoneNumber());
+		client.setStreetName(
+				client.getStreetName() == null ? clientToBeModified.getStreetName() : client.getStreetName());
+		client.setStreetNumber(
+				client.getStreetNumber() == null ? clientToBeModified.getStreetNumber() : client.getStreetNumber());
+		client.setCep(client.getCep() == null ? clientToBeModified.getCep() : client.getCep());
+
 		return this.clientRepository.save(client);
 	}
-	
+
 }
